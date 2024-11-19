d466d225ab5fc589c8b4d31bb17e233fbd3a2cc4
yusung103 <gimyuseong742@gmail.com>
2024-11-19 23:54:17 +0900

    add :: reader, processor, writer

---
+++ SpringBatch/src/main/java/org/example/springbatch/batch/FirstBatch.java
@@ -0,0 +1,87 @@
+package org.example.springbatch.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbatch.entity.AfterEntity;
+import org.example.springbatch.entity.BeforeEntity;
+import org.example.springbatch.repository.AfterRepository;
+import org.example.springbatch.repository.BeforeRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirstBatch {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    private final BeforeRepository beforeRepository;
+    private final AfterRepository afterRepository;
+
+    @Bean
+    public Job firstJob() {
+
+        return new JobBuilder("firstJob", jobRepository)
+                .start(firstStep())
+                .build();
+    }
+
+    @Bean
+    public Step firstStep() {
+        return new StepBuilder("firstStep", jobRepository)
+                .<BeforeEntity, AfterEntity> chunk(10, platformTransactionManager)
+                .reader(beforeReader())
+                .processor(middleProcessor())
+                .writer(afterWriter())
+                .build();
+    }
+
+    @Bean
+    public RepositoryItemReader<BeforeEntity> beforeReader(){
+        return new RepositoryItemReaderBuilder<BeforeEntity>()
+                .name("BeforeReader")
+                .pageSize(10)
+                .methodName("before")
+                .methodName("findAll")
+                .repository(beforeRepository)
+                .sorts(Map.of("id", Sort.Direction.ASC))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<BeforeEntity, AfterEntity> middleProcessor(){
+        return new ItemProcessor <BeforeEntity, AfterEntity>() {
+
+            @Override
+            public AfterEntity process(BeforeEntity item) throws Exception {
+
+                AfterEntity afterEntity = new AfterEntity();
+                afterEntity.setUsername(item.getUsername());
+
+                return afterEntity;
+            }
+        };
+    }
+
+    @Bean
+    public RepositoryItemWriter<AfterEntity> afterWriter(){
+
+        return new RepositoryItemWriterBuilder<AfterEntity>()
+                .repository(afterRepository)
+                .methodName("save")
+                .build();
+    }
+}
